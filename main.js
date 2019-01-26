768499b285c4921d5c7df03fa530be4cbf841798
Adrian <Adrian_Avram@Outlook.com>
2019-01-26 15:24:07 -0800

    Toggle seconds by pressing 't'

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function showTime() {
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
+    
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
 
@@ -39,7 +40,13 @@ function showTime() {
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time;
+    if (localStorage.getItem("showSec") === "true") {
+        time = h + ":" + m + ":" + s + " " + session;
+    } else {
+        time = h + ":" + m + " " + session;
+    }
+
     document.getElementById("ClockDisplay").innerText = time;
     document.getElementById("ClockDisplay").textContent = time;
 }
@@ -89,4 +96,13 @@ function fade(element, element2) {
         element2.style.opacity = op;
         op = op + 0.05;
     }, 30);
+}
+
+document.onkeypress = function (e) {
+    e = e || window.event
+    if (e.key === 't') {
+        localStorage.setItem("showSec", !(localStorage.getItem("showSec") === "true"))
+        console.log(localStorage.getItem("showSec"))
+        showTime()
+    }
 }
