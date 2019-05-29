2ff89094003a83f9609d96d5e2aa61766c675327
Adrian Avram <Adrian_Avram@Outlook.com>
2019-05-28 19:54:38 -0700

    Update to fetch

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-function getJSON(url) {
-    var resp;
-    var xmlHttp;
-
-    resp = "";
-    xmlHttp = new XMLHttpRequest();
-
-    if (xmlHttp != null) {
-        xmlHttp.open("GET", url, false);
-        xmlHttp.send(null);
-        resp = xmlHttp.responseText;
-    }
-    return resp;
-}
-
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -25,7 +10,7 @@ function showTime() {
     var d = new Date();
     var s = checkTime(d.getSeconds());
     var m = checkTime(d.getMinutes());
-    var h = d.getHours();
+    var h = checkTime(d.getHours());
 
     if (localStorage.getItem("showSec") === "true")
         clock.innerText = h + ":" + m + ":" + s;
@@ -34,7 +19,7 @@ function showTime() {
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};
+    if (i < 10) { i = "0" + i };
     return i;
 }
 
@@ -42,23 +27,25 @@ function checkTime(i) {
 showTime();
 setInterval(showTime, 1000);
 
-var gjson;
+var json;
 var links = [[]];
 
 if (localStorage.getItem("lastFetch") === null || parseFloat(localStorage.getItem("lastFetch")) + 18000000 < + new Date()) {
-    gjson = JSON.parse(getJSON("https://www.reddit.com/r/earthporn.json"));
-    localStorage.setItem("lastFetch", + new Date());
-    console.log("fetched")
-    var index = 0;
-    gjson.data.children.forEach(function (element) {
-            if (!element.data.stickied && element.data.preview.images[0].source.width / element.data.preview.images[0].source.height > 1.3 && element.data.preview.images[0].source.width > 1400) {
-                if (!links[index]) links[index] = []
-                links[index][0] = element.data.url;
-                links[index][1] = element.data.permalink;
-                index++;
-            }
-    });
-    localStorage.setItem("linksArr", JSON.stringify(links))
+    fetch("https://www.reddit.com/r/earthporn.json").then(res => res.json())
+        .then((json) => {
+            localStorage.setItem("lastFetch", + new Date());
+            console.log("fetched")
+            var index = 0;
+            json.data.children.forEach(function (element) {
+                if (!element.data.stickied && element.data.preview.images[0].source.width / element.data.preview.images[0].source.height > 1.3 && element.data.preview.images[0].source.width > 1400) {
+                    if (!links[index]) links[index] = []
+                    links[index][0] = element.data.url;
+                    links[index][1] = element.data.permalink;
+                    index++;
+                }
+            });
+            localStorage.setItem("linksArr", JSON.stringify(links));
+        });
 } else {
     links = JSON.parse(localStorage.getItem("linksArr"));
 }
