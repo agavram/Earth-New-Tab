7e112fa560d97d081d36211f4a27d33f6b788e88
Adrian Avram <Adrian_Avram@Outlook.com>
2019-06-30 11:28:09 -0400

    Fixed bugs

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ setInterval(showTime, 1000);
 var json;
 var links = [[]];
 
-if (localStorage.getItem("lastFetch") === null || parseFloat(localStorage.getItem("lastFetch")) + 18000000 < + new Date()) {
-    fetch("https://www.reddit.com/r/earthporn.json").then(res => res.json())
+if (localStorage.getItem("linksArr") === null || localStorage.getItem("lastFetch") === null || parseFloat(localStorage.getItem("lastFetch")) + 18000000 < + new Date()) {
+    fetch("https://www.reddit.com/r/EarthPorn/top/.json?t=day").then(res => res.json())
         .then((json) => {
             localStorage.setItem("lastFetch", + new Date());
             console.log("fetched")
@@ -39,45 +39,53 @@ if (localStorage.getItem("lastFetch") === null || parseFloat(localStorage.getIte
             json.data.children.forEach(function (element) {
                 if (!element.data.stickied && element.data.preview.images[0].source.width / element.data.preview.images[0].source.height > 1.3 && element.data.preview.images[0].source.width > 1400) {
                     if (!links[index]) links[index] = []
-                    links[index][0] = element.data.url;
+                    let newURL = element.data.url;
+                    if (newURL.includes("imgur")) {
+                        newURL += ".jpg"
+                    }
+                    links[index][0] = newURL;
                     links[index][1] = element.data.permalink;
                     index++;
                 }
             });
             localStorage.setItem("linksArr", JSON.stringify(links));
+            displayImage();
         });
 } else {
     links = JSON.parse(localStorage.getItem("linksArr"));
+    displayImage();
 }
 
-var rand = getRandomInt(0, links.length - 1);
-var url = links[rand][0];
-var img = document.getElementById("image");
-img.onload = function () {
-    fade(img, document.getElementById("ClockDisplay"))
-}
-img.src = url;
-
-document.getElementById("reddit_link").href = "https://www.reddit.com" + links[rand][1];
-
-function fade(element, element2) {
-    var op = 0.01; // initial opacity
-    var timer = setInterval(function () {
-        if (op >= 1.0) {
-            clearInterval(timer);
-            showTime();
+function displayImage() {
+    var rand = getRandomInt(0, links.length - 1);
+    var url = links[rand][0];
+    var img = document.getElementById("image");
+    img.onload = function () {
+        fade(img, document.getElementById("ClockDisplay"))
+    }
+    img.src = url;
+    
+    document.getElementById("reddit_link").href = "https://www.reddit.com" + links[rand][1];
+    
+    function fade(element, element2) {
+        var op = 0.01; // initial opacity
+        var timer = setInterval(function () {
+            if (op >= 1.0) {
+                clearInterval(timer);
+                showTime();
+            }
+            element.style.opacity = op;
+            element2.style.opacity = op;
+            op = op + 0.05;
+        }, 30);
+    }
+    
+    document.onkeypress = function (e) {
+        e = e || window.event
+        if (e.key === 't') {
+            localStorage.setItem("showSec", !(localStorage.getItem("showSec") === "true"))
+            console.log(localStorage.getItem("showSec"))
+            showTime()
         }
-        element.style.opacity = op;
-        element2.style.opacity = op;
-        op = op + 0.05;
-    }, 30);
-}
-
-document.onkeypress = function (e) {
-    e = e || window.event
-    if (e.key === 't') {
-        localStorage.setItem("showSec", !(localStorage.getItem("showSec") === "true"))
-        console.log(localStorage.getItem("showSec"))
-        showTime()
     }
 }
